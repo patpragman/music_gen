a12f806df8d4053a770e2343c35f25de36cf22c0
Patrick Pragman <patrickpragman@pop-os.localdomain>
2022-12-14 12:30:44 -0900

    initial test of the synth library

---
+++ docs/player_docs/initial_research.md
@@ -0,0 +1,5 @@
+
+https://www.reddit.com/r/Python/comments/4796gy/best_midi_libraries_for_python/
+
+https://pypi.org/project/synthesizer/
+
---
+++ player/player.py
@@ -0,0 +1,13 @@
+
+
+# initial test of the synthesizer and pyaudio libraries
+
+from synthesizer import Player, Synthesizer, Waveform
+
+
+player = Player()
+player.open_stream()
+synthesizer = Synthesizer(osc1_waveform=Waveform.sine, osc1_volume=1.0, use_osc2=False)
+
+# Play A4
+player.play_wave(synthesizer.generate_constant_wave(440.0, 3.0))
